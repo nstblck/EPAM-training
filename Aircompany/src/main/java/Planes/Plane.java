19ebb42e63f60ed2a67e9046105b27b030003e06
AnastasiyaM <Anhysteric@gmail.com>
2021-04-20 19:01:02 +0300

    no message

---
+++ Aircompany/src/main/java/Planes/Plane.java
@@ -0,0 +1,62 @@
+package Planes;
+
+import java.util.Objects;
+
+abstract public class Plane {
+    String model;
+    private int maxSpeed;
+    private int maxFlightDistance;
+    private int maxLoadCapacity;
+
+    public Plane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.maxFlightDistance = maxFlightDistance;
+        this.maxLoadCapacity = maxLoadCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMaxSpeedS() {
+        return maxSpeed;
+    }
+
+    public int GetMaxFlightDistance() {
+        return maxFlightDistance;
+    }
+
+    public int getMinLoadCapacity() {
+//        int result = this.maxLoadCapacity;
+//        return result;
+        int maxLoadCapacity = this.maxLoadCapacity;
+        return maxLoadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{" + "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", maxFlightDistance=" + maxFlightDistance +
+                ", maxLoadCapacity=" + maxLoadCapacity + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //if (this == o) return true;
+        this.equals(o);
+        //if (!(o instanceof Plane)) return false; заменила на ниже
+        if (o instanceof Plane) return true;
+        Plane plane = (Plane) o;
+        return maxSpeed == plane.maxSpeed &&
+                maxFlightDistance == plane.maxFlightDistance &&
+                maxLoadCapacity == plane.maxLoadCapacity &&
+                Objects.equals(model, plane.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
