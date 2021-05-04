4bec7568be32f573fbff8bd2c1b9aa5420d290f1
AnastasiyaM <Anhysteric@gmail.com>
2021-05-04 23:43:12 +0300

    no message

--- Task Fundamentals/src/com/epam_training/Main.java
+++ Task Fundamentals/src/com/epam_training/Main.java
@@ -7,18 +7,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Приветствовать любого пользователя при вводе его имени через командную строку.
         System.out.println("Введите ваше имя");
+        sayHello();
+        System.out.println("Введите аргументы");
+        reverseArguments();
+        System.out.println("Введите количество случайных чисел");
+        randomNumbers();
+        System.out.println("Введите целые числа для подсчета суммы и произведения");
+        getSumAndProduct();
+        System.out.println("Введите число");
+        getMonth();
+    }
+
+
+    public static void sayHello() {
+        Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Здравствуйте, " + name);
-        // Отобразить в окне консоли аргументы командной строки в обратном порядке.
-        System.out.println("Введите аргументы");
+    }
+
+    public static void reverseArguments() {
+        Scanner scanner = new Scanner(System.in);
         String arguments = scanner.nextLine();
         String argumentsReverse = new StringBuffer(arguments).reverse().toString();
         System.out.println(argumentsReverse);
-        //Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+    }
+
+    public static void randomNumbers() {
         Random random = new Random();
-        System.out.println("Введите количество случайных чисел");
+        Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
         int maxValue = Integer.MAX_VALUE;
         int i;
@@ -27,11 +44,60 @@ public class Main {
             System.out.println(y); //с переходом на новую строку
             // System.out.print(y+" "); без перехода на новую строку
         }
-        //Ввести целые числа как аргументы командной строки,
-        // подсчитать их сумму (произведение) и вывести результат на консоль.
+    }
+
+    public static void getMonth() {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfMonth = scanner.nextInt();
+        if (numberOfMonth > 12 || numberOfMonth <= 0) {
+            System.out.println("Incorrect");
+        } else {
+            switch (numberOfMonth) {
+                case 1:
+                    System.out.println("Jan");
+                    break;
+                case 2:
+                    System.out.println("Feb");
+                    break;
+                case 3:
+                    System.out.println("March");
+                    break;
+                case 4:
+                    System.out.println("Apr");
+                    break;
+                case 5:
+                    System.out.println("May");
+                    break;
+                case 6:
+                    System.out.println("Jun");
+                    break;
+                case 7:
+                    System.out.println("Jul");
+                    break;
+                case 8:
+                    System.out.println("Aug");
+                    break;
+                case 9:
+                    System.out.println("Sept");
+                    break;
+                case 10:
+                    System.out.println("Oct");
+                    break;
+                case 11:
+                    System.out.println("Nov");
+                    break;
+                case 12:
+                    System.out.println("Dec");
+                    break;
+            }
+        }
+    }
+
+
+    public static void getSumAndProduct() {
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int product = 1;
-        System.out.println("Введите целые числа для подсчета суммы и произведения");
         while (scanner.hasNextInt()) {
             int summand = scanner.nextInt();
             sum = sum + summand;
@@ -40,38 +106,5 @@ public class Main {
         scanner.nextLine();
         System.out.println("Сумма = " + sum);
         System.out.println("Произведение = " + product);
-
-        //Ввести число от 1 до 12. Вывести на консоль название месяца,
-        // соответствующего данному числу. Осуществить проверку корректности ввода чисел.
-        System.out.println("Введите число");
-        int numberOfMonth = scanner.nextInt();
-        if (numberOfMonth > 12 || numberOfMonth <= 0) {
-            System.out.println("Incorrect");
-        } else if (numberOfMonth == 1) {
-            System.out.println("Jan");
-        } else if (numberOfMonth == 2) {
-            System.out.println("Feb");
-        } else if (numberOfMonth == 3) {
-            System.out.println("March");
-        } else if (numberOfMonth == 4) {
-            System.out.println("Apr");
-        } else if (numberOfMonth == 5) {
-            System.out.println("May");
-        } else if (numberOfMonth == 6) {
-            System.out.println("Jun");
-        } else if (numberOfMonth == 7) {
-            System.out.println("Jul");
-        } else if (numberOfMonth == 8) {
-            System.out.println("Aug");
-        } else if (numberOfMonth == 9) {
-            System.out.println("Sept");
-        } else if (numberOfMonth == 10) {
-            System.out.println("Oct");
-        } else if (numberOfMonth == 11) {
-            System.out.println("Nov");
-        } else {
-            System.out.println("Dec");
-        }
     }
 }
-
